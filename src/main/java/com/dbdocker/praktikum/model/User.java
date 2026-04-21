791636090164185913ca22718ecc180a0f896ae3
sulthann1 <sulthanaawaliya155@gmail.com>
2026-04-21 09:37:49 +0700

    Initial commit, Membuat semua package dan java class user

---
+++ src/main/java/com/dbdocker/praktikum/model/User.java
@@ -0,0 +1,30 @@
+package com.dbdocker.praktikum.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "20240140087_users")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String nim;
+
+}
---
+++ src/test/java/com/dbdocker/praktikum/PraktikumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbdocker.praktikum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraktikumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
